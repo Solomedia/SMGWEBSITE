0e3401a58e6417863301fe76185a0973452cc2f0
Richarson Castro <richars@ula.ve>
2019-05-25 20:49:51 -0500

    Updated Site Metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`
+		title: `Solo Media Group`,
+		description: `Solo Media Group- We help brands make money and look good doing it.`,
+		author: `Solo Media Group`
 	},
 	plugins: [
 		{
