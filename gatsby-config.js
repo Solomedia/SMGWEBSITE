6ed79c3e39b3d4944066c464eec11a58eb447ee3
jvaldezq <jvaldezq1101@gmail.com>
2019-07-04 14:56:52 -0600

    Adding google analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,23 @@ module.exports = {
 			}
 		},
 		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-89457608-3',
+				// Defines where to place the tracking script - `true` in the head and `false` in the body
+				head: false,
+				// Setting this parameter is optional
+				anonymize: true,
+				// Setting this parameter is also optional
+				respectDNT: true,
+				// Avoids sending pageview hits from custom paths
+				exclude: ['/preview/**', '/do-not-track/me/too/'],
+				sampleRate: 5,
+				siteSpeedSampleRate: 10,
+				cookieDomain: 'solomediagroup.co'
+			}
+		},
+		{
 			resolve: `gatsby-source-wordpress`,
 			options: {
 				/*
